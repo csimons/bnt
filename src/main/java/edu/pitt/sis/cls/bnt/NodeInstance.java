67a40f72d3b77e14344c5dd69c979d777173edd6
Christopher Simons <csimons@users.noreply.github.com>
2013-03-07 11:41:06 -0500

    Added properties, Javadoc to NodeInstance.

--- src/main/java/edu/pitt/sis/cls/bnt/NodeInstance.java
+++ src/main/java/edu/pitt/sis/cls/bnt/NodeInstance.java
@@ -3,7 +3,28 @@ package edu.pitt.sis.cls.bnt;
 import edu.pitt.sis.cls.bnt.lang.Cpt;
 
 public class NodeInstance {
+	/**
+	 * Unique, alphanumeric identifier.
+	 */
 	public String id;
+
+	/**
+	 * Full display name (i.e., "anne::child[d=0]::mother[d=1]")
+	 */
 	public String name;
+
+	/**
+	 * Conditional probability table; a-prior tables will lack "given" property.
+	 */
 	public Cpt cpt;
+
+	/**
+	 * Domain slice from which this node was instantiated.
+	 */
+	public int domainSlice;
+
+	/**
+	 * Template node name from which this node was instantiated.
+	 */
+	public String templateNodeName;
 }
