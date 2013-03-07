92d8e13eaaa54295e989cf69166130ef5bd93d50
Christopher Simons <csimons@users.noreply.github.com>
2013-03-07 11:05:44 -0500

    Began test implementation of BN generation.

--- src/main/java/edu/pitt/sis/cls/bnt/Generator.java
+++ src/main/java/edu/pitt/sis/cls/bnt/Generator.java
@@ -1,11 +1,16 @@
 package edu.pitt.sis.cls.bnt;
 
-import edu.pitt.sis.cls.bnt.lang.Bnt;
-
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+
+import edu.pitt.sis.cls.bnt.lang.Binding;
+import edu.pitt.sis.cls.bnt.lang.Bnt;
+import edu.pitt.sis.cls.bnt.lang.Node;
+import edu.pitt.sis.cls.bnt.lang.Slice;
 
 public class Generator {
     private Map<String, Class<? extends Parser>> parsers;
@@ -21,6 +26,28 @@ public class Generator {
         Parser parser = parsers.get(extension(sourceFilename)).newInstance();
         Bnt bnt = parser.parse(is);
         is.close();
+
+        List<List<NodeInstance>> domainLayers = new LinkedList<List<NodeInstance>>();
+        for (Slice slice : bnt.getDomain().getSlice()) {
+            List<NodeInstance> domainLayer = new LinkedList<NodeInstance>();
+            for (Node templateNode : bnt.getTemplate().getNode()) {
+                NodeInstance nodeInstance = new NodeInstance();
+                Binding templateBinding = templateNode.getBinding();
+                for (int i = 0; i < slice.getBinding().size(); i += 1) {
+                    Binding domainBinding = slice.getBinding().get(i);
+                    if (!domainBinding.getSymbol().equals(templateBinding.getSymbol()))
+                        continue;
+                    else {
+                        nodeInstance.name = String.format("%s::%s[d=%d]",
+                            domainBinding.getValue(),
+                            templateNode.getName(), i);
+                        domainLayer.add(nodeInstance);
+                    }
+                }
+            }
+            domainLayers.add(domainLayer);
+        }
+        // TODO: Collapse domain layers.
     }
 
     private String extension(String filename) {
---
+++ src/main/java/edu/pitt/sis/cls/bnt/NodeInstance.java
@@ -0,0 +1,9 @@
+package edu.pitt.sis.cls.bnt;
+
+import edu.pitt.sis.cls.bnt.lang.Cpt;
+
+public class NodeInstance {
+    public String id;
+    public String name;
+    public Cpt cpt;
+}
